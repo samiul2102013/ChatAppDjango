5b6383718c4fef613cd90a664ce0c1e5973b22b0
samiul2102013 <samiulhasan0186@gmail.com>
2025-04-23 00:44:55 +0600

    all

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -37,9 +37,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fv@!-bs_yfk2!)c!8%$=qvhyxd)yfbo4z6cz&(eo@v6p2es7@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
